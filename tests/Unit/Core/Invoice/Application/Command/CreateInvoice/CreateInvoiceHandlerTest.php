e8c482ebe476d122f36469828d30ed9062a9c1c8
Adam <adamdawidgula@gmail.com>
2024-11-19 23:26:34 +0100

    fix tests

--- tests/Unit/Core/Invoice/Application/Command/CreateInvoice/CreateInvoiceHandlerTest.php
+++ tests/Unit/Core/Invoice/Application/Command/CreateInvoice/CreateInvoiceHandlerTest.php
@@ -5,71 +5,40 @@ namespace App\Tests\Unit\Core\Invoice\Application\Command\CreateInvoice;
 use App\Core\Common\Domain\ValueObject\Email;
 use App\Core\Invoice\Application\Command\CreateInvoice\CreateInvoiceCommand;
 use App\Core\Invoice\Application\Command\CreateInvoice\CreateInvoiceHandler;
-use App\Core\Invoice\Domain\Invoice;
-use App\Core\Invoice\Domain\Repository\InvoiceWriteRepositoryInterface;
+use App\Core\Invoice\Domain\Feature\InvoiceCreationInterface;
 use App\Core\Invoice\Domain\ValueObject\Amount;
-use App\Core\User\Domain\Exception\UserNotFoundException;
-use App\Core\User\Domain\Repository\UserWriteRepositoryInterface;
-use App\Core\User\Domain\User;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Uid\Ulid;
 
 final class CreateInvoiceHandlerTest extends TestCase
 {
-    private UserWriteRepositoryInterface&MockObject $userRepository;
-
-    private InvoiceWriteRepositoryInterface&MockObject $invoiceRepository;
+    private InvoiceCreationInterface&MockObject $invoiceCreator;
 
     private CreateInvoiceHandler $handler;
 
     protected function setUp(): void
     {
-        parent::setUp();
-
         $this->handler = new CreateInvoiceHandler(
-            $this->invoiceRepository = $this->createMock(
-                InvoiceWriteRepositoryInterface::class
-            ),
-            $this->userRepository = $this->createMock(
-                UserWriteRepositoryInterface::class
+            $this->invoiceCreator = $this->createMock(
+                InvoiceCreationInterface::class
             )
         );
     }
 
-    public function test_handle_success(): void
+    public function testHandleSuccess(): void
     {
-        $user = new User(new Ulid(), new Email('test@email.com'));
-        $invoice = new Invoice(new Ulid(), $user, new Amount(12500));
-
-        $this->userRepository->expects(self::once())
-            ->method('findByEmail')
-            ->willReturn($user);
-
-        $this->invoiceRepository->expects(self::once())
-            ->method('save')
-            ->with($invoice);
-
-        $this->invoiceRepository->expects(self::once())
-            ->method('flush');
-
-        $this->handler->__invoke(new CreateInvoiceCommand(
-            $invoice->getId(),
-            $invoice->getUser()->getEmail(),
-            $invoice->getAmount(),
-        ));
-    }
-
-    public function test_handle_user_not_exists(): void
-    {
-        $this->expectException(UserNotFoundException::class);
-
-        $this->userRepository->expects(self::once())
-            ->method('findByEmail')
-            ->willThrowException(new UserNotFoundException());
-
-        $this->handler->__invoke(
-            new CreateInvoiceCommand(new Ulid(), new Email('test@email.com'), new Amount(1))
+        $command = new CreateInvoiceCommand(
+            new Ulid(),
+            new Email('test@email.com'),
+            new Amount(12500),
         );
+
+        $this->invoiceCreator
+            ->expects(self::once())
+            ->method('createInvoice')
+            ->with($command->id, $command->userEmail, $command->amount);
+
+        $this->handler->__invoke($command);
     }
 }
--- tests/Unit/Core/User/Application/Command/CreateUser/CreateUserHandlerTest.php
+++ tests/Unit/Core/User/Application/Command/CreateUser/CreateUserHandlerTest.php
@@ -7,53 +7,30 @@ namespace App\Tests\Unit\Core\User\Application\Command\CreateUser;
 use App\Core\Common\Domain\ValueObject\Email;
 use App\Core\User\Application\Command\CreateUser\CreateUserCommand;
 use App\Core\User\Application\Command\CreateUser\CreateUserHandler;
-use App\Core\User\Domain\Repository\UserWriteRepositoryInterface;
-use App\Core\User\Domain\User;
+use App\Core\User\Domain\Feature\UserCreationInterface;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Uid\Ulid;
 
 final class CreateUserHandlerTest extends TestCase
 {
-    private UserWriteRepositoryInterface&MockObject $userRepository;
+    private UserCreationInterface&MockObject $userCreator;
     private CreateUserHandler $handler;
 
     protected function setUp(): void
     {
-        $this->userRepository = $this->createMock(UserWriteRepositoryInterface::class);
-        $this->handler = new CreateUserHandler($this->userRepository);
+        $this->userCreator = $this->createMock(UserCreationInterface::class);
+        $this->handler = new CreateUserHandler($this->userCreator);
     }
 
-    public function testInvokeShouldProperlySavesAndFlushUser(): void
+    public function testHandleSuccess(): void
     {
         $command = new CreateUserCommand(new Ulid(), new Email('test@email.com'));
 
-        $this->userRepository
+        $this->userCreator
             ->expects(self::once())
-            ->method('save')
-            ->with(new User($command->id, $command->email));
-
-        $this->userRepository
-            ->expects(self::once())
-            ->method('flush');
-
-        $this->handler->__invoke($command);
-    }
-
-    public function testInvokeShouldFailWhenUserWithGivenEmailAlreadyExists(): void
-    {
-        $this->markTestSkipped('There is no validation yet.');
-        $command = new CreateUserCommand(new Ulid(), new Email('test@email.com'));
-
-        $this->userRepository
-            ->expects(self::never())
-            ->method('save');
-
-        $this->userRepository
-            ->expects(self::never())
-            ->method('flush');
-
-        $this->expectException(\Exception::class);
+            ->method('createUser')
+            ->with($command->id, $command->email);
 
         $this->handler->__invoke($command);
     }
